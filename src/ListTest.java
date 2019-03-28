86060170a6f99ff09a015e1d624b1b8fbd7a36c9
Keith Chappelow <Keith.Chappelow@dematic.com>
2019-03-28 12:14:58 -0400

    Add starter code.

---
+++ src/ListTest.java
@@ -0,0 +1,42 @@
+// Fig. 21.4: ListTest.java
+// ListTest class to demonstrate List capabilities.
+import com.deitel.datastructures.List;
+import java.util.NoSuchElementException;
+
+public class ListTest {
+   public static void main(String[] args) {
+      List<Integer> list = new List<>();
+
+      // insert integers in list
+      list.insertAtFront(-1);
+      list.print();
+      list.insertAtFront(0);
+      list.print();
+      list.insertAtBack(1);
+      list.print();
+      list.insertAtBack(5);
+      list.print();
+
+      // remove objects from list; print after each removal
+      try {
+         int removedItem = list.removeFromFront();
+         System.out.printf("%n%d removed%n", removedItem);
+         list.print();
+
+         removedItem = list.removeFromFront();
+         System.out.printf("%n%d removed%n", removedItem);
+         list.print();
+
+         removedItem = list.removeFromBack();
+         System.out.printf("%n%d removed%n", removedItem);
+         list.print();
+
+         removedItem = list.removeFromBack();
+         System.out.printf("%n%d removed%n", removedItem);
+         list.print();
+      }
+      catch (NoSuchElementException noSuchElementException) {
+         noSuchElementException.printStackTrace();
+      }
+   }
+}
